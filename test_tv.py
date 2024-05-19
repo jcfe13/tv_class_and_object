c4c09da22e067809a0e03e78d5f43003cb391a92
jcfe13 <johncarloesquillo72@gmail.com>
2024-05-19 22:54:51 +0800

    Pseudocode

--- test_tv.py
+++ test_tv.py
@@ -6,10 +6,9 @@ def test_tv():
     tv1 = TV()
     tv2 = TV()
 
-    tv1.turn_on()
-    tv1.set_channel(30)
-    tv1.set_volume(3)
-
     tv2.turn_on()
     tv2.set_channel(3)
     tv2.set_volume(2)
+
+    # 4. Print the channel and volume level for both TVs.
+    
