c55fd34092a656c1ab59b5279cdb77daf94eb957
jcfe13 <johncarloesquillo72@gmail.com>
2024-05-19 22:55:55 +0800

    Turn on TV1, set it's channel to 30, and volume level to 3

--- test_tv.py
+++ test_tv.py
@@ -6,9 +6,6 @@ def test_tv():
     tv1 = TV()
     tv2 = TV()
 
-    tv2.turn_on()
-    tv2.set_channel(3)
-    tv2.set_volume(2)
-
-    # 4. Print the channel and volume level for both TVs.
-    
+    tv1.turn_on()
+    tv1.set_channel(30)
+    tv1.set_volume(3)
