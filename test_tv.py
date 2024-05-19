57e9dbf0881f9ed36b21e4650f9e48808d704ecb
jcfe13 <johncarloesquillo72@gmail.com>
2024-05-19 23:04:47 +0800

    Print the channel and volume level for TV1 and TV2

--- test_tv.py
+++ test_tv.py
@@ -15,4 +15,6 @@ def test_tv():
     tv2.set_volume(2)
 
     # 4. Print the channel and volume level for both TVs.
+    print(f"\ntv1's channel is {tv1.get_channel()} and volume level is {tv1.get_volume()}")
+    print(f"tv2's channel is {tv2.get_channel()} and volume level is {tv2.get_volume()}")
     
